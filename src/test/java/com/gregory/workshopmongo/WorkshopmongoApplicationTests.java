bedf15e3cac156bfb7cdc2f376575bf37fe65b75
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-03-15 12:17:56 -0300

    primeiro commit

---
+++ src/test/java/com/gregory/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregory.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
