9d96e998cf63f0ced3c499bde1eff5d3e02f9a17
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-03-16 15:11:11 -0300

    Referenciando os posts do usuário

--- src/main/java/com/gregory/workshopmongo/domain/User.java
+++ src/main/java/com/gregory/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.gregory.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -13,6 +16,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // anotação para informar que é uma lista de referencia aos posts 
+	// lazy para que os posts sejam carregados apenas se forem acessados, para não gerar um volume alto de transações trazendo cada post que cada usuario tem 
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -46,6 +53,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
