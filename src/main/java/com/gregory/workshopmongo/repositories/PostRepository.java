b24486c52088b104ac9844a907ab41990edd2e81
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-03-16 16:00:56 -0300

    Consulta simples com @Query

--- src/main/java/com/gregory/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/gregory/workshopmongo/repositories/PostRepository.java
@@ -3,12 +3,17 @@ package com.gregory.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gregory.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//consulta simple com o @Query usando o formato do Json padrão do mongoDB para realizar a consulta
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	//Metodo query que busca no campo title um texto recebido //IgnoreCase para ignorar o maiusculo e minusculo 
 	List<Post> findByTitleContainingIgnoreCase(String text);
